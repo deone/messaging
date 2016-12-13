44899af6a107b6d7af78e764c8cc9f5d846ddb64
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-13 21:42:32 +0000

    Attach file to email before sending.

--- app/views.py
+++ app/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.http import JsonResponse
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.views.decorators.csrf import ensure_csrf_cookie
+
+import requests
+import os
 
 @ensure_csrf_cookie
 def index(request):
     data = dict(request.GET)
-    send_mail(
+    file = data['file'][0]
+    file_name = file.split('/')[-1]
+
+    response = requests.get(file)
+    with open(file_name, 'a') as f:
+        f.write(response.content)
+
+    email = EmailMessage(
         data['subject'][0],
         data['message'][0],
         data['sender'][0],
         data['recipients'],
-        fail_silently=False,
     )
+    file = os.path.join(settings.BASE_DIR, file_name)
+    email.attach_file(file)
+    email.send()
+
     return JsonResponse({'status': 'ok'})
