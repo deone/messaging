35af54418055fc11f13eed61b6d30ef9b798e36b
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-13 19:34:05 +0000

    Receive request and send mail.

--- app/views.py
+++ app/views.py
@@ -5,11 +5,12 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
 def index(request):
+    data = dict(request.GET)
     send_mail(
-        'Subject here',
-        'Here is the message.',
-        'incisiaappmailer@gmail.com',
-        ['alwaysdeone@gmail.com'],
+        data['subject'][0],
+        data['message'][0],
+        data['sender'][0],
+        data['recipients'],
         fail_silently=False,
     )
     return JsonResponse({'status': 'ok'})
